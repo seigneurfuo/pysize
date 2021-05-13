962652de9f0f17318e97bc6498961e891b40b067
seigneurfuo <seigneurfuo@protonmail.com>
2021-05-14 00:33:37 +0200

    Corrections diverses

--- pysize.py
+++ pysize.py
@@ -4,10 +4,14 @@ import shutil
 import os
 import json
 
+# Version 0.3
+
 class PySize():
     def __init__(self):
         self.json_data = {}
         self.json_data_path = os.path.join(os.path.dirname(__file__), "pysize-data.json")
+        self.date_format = "%Y-%m-%d-%H:%M:%S"
+        self.output_date_format = "%d/%m/%Y %H:%M:%S"
 
     def load(self):
         with open(self.json_data_path, "r") as json_data:
@@ -20,44 +24,42 @@ class PySize():
     def add_record(self):
         """ In Bytes"""
 
-        date = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+        date = datetime.now().strftime(self.date_format)
         total, used, free = shutil.disk_usage(self.json_data["config"]["root"])
         #unit = self.json_data["config"]["unit"]
 
-        # Bits par GigaByte
-        bytes_per_GB = 1024 * 1024 * 1024
-
-        total /= bytes_per_GB
-        used /= bytes_per_GB
-        free /= bytes_per_GB
-
-
-        record = {"unit": self.json_data["config"]["unit"],
+        record = {"unit": "bytes",
                   "date": date,
-                  "total": total,
-                  "used": used,
-                  "free": free
+                  "bytes_total": total,
+                  "bytes_used": used,
+                  "bytes_free": free
                   }
 
         self.json_data["data"].append(record)
         return record
 
 
-    def export_to_csv(self):
+    def export_to_csv(self, filename="export.csv"):
         if not self.json_data:
             Exception("Please load the data file before !")
 
-        with open("export.csv", 'w', newline='') as csv_file:
+        filepath = os.path.join(os.path.dirname(__file__), filename)
+
+        with open(filename, 'w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            fields = ["Total", "Used", "Free", "Unit"]
+            fields = ["Date", "Total", "Used", "Free", "Unit"]
             writer = csv.DictWriter(csv_file, fieldnames=fields)
 
             writer.writeheader()
             for record in self.json_data["data"]:
-                row_data = [record["date"],
-                            record["total"],
-                            record["used"],
-                            record["free"],
+
+                date_object = datetime.strptime(record["date"], self.date_format)
+                date = date_object.strftime(self.output_date_format)
+
+                row_data = [date,
+                            record["bytes_total"],
+                            record["bytes_used"],
+                            record["bytes_free"],
                             record["unit"]]
 
                 csv_writer.writerow(row_data)
@@ -65,9 +67,45 @@ class PySize():
             msg = "{} row(s) exported.".format(len(self.json_data["data"]))
             print(msg)
 
+
+    def export_to_chart(self, filename="chart.png"):
+        if not self.json_data:
+            Exception("Please load the data file before !")
+
+        filepath = os.path.join(os.path.dirname(__file__), filename)
+
+        from matplotlib import pyplot
+
+        x_date = [] # Date
+        y_used = [] # Used
+        y_total = [] # Total
+
+        for index, record in enumerate(self.json_data["data"]):
+
+            date_object = datetime.strptime(record["date"], self.date_format)
+            date = date_object.strftime(self.output_date_format)
+
+            used = (record["bytes_used"] / 8) / (1024 ^ 3)
+            total = (record["bytes_total"] / 8) / (1024 ^ 3)
+
+            x_date.append(date)
+            y_used.append(used)
+            y_total.append(total)
+
+        # Draw lines
+        pyplot.plot(x_date, y_total, linestyle="-", marker="o", color="b")
+        pyplot.plot(x_date, y_used, linestyle="-", marker="o", color="r")
+
+        # Force 0 to show in graph
+        pyplot.ylim(ymin=0)
+        pyplot.xticks(rotation=90)
+        pyplot.tight_layout() # Avoid xticks to be outside of the image
+        pyplot.savefig(filepath)
+
 if __name__ == "__main__":
     pysize = PySize()
     pysize.load()
     pysize.add_record()
     pysize.save()
-    #pysize.export_to_csv()
+    pysize.export_to_csv()
+    #pysize.export_to_chart()
